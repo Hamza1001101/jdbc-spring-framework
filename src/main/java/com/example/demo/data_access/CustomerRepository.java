5af9d6ebedc67e798a1ff447a7076cad4ddf3cbd
Hamza Farah <hamza.farah@se.experis.com>
2021-07-20 13:00:40 +0200

    added the database and connected to the jdbc

---
+++ src/main/java/com/example/demo/data_access/CustomerRepository.java
@@ -0,0 +1,63 @@
+package com.example.demo.data_access;
+
+import com.example.demo.models.Customers;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+
+public class CustomerRepository {
+    private static final String URL = "jdbc:sqlite::resource:Chinook_Sqlite.sqlite";
+    private static ArrayList<com.example.demo.models.Customers> customers = new ArrayList<Customers>();
+    private static Connection conn = null;
+
+    public ArrayList<Customers> selectAllCustomers() {
+
+        try {
+            //Connect to DB
+            conn = DriverManager.getConnection(URL);
+
+            //Make SQL Query
+            PreparedStatement ps = conn.prepareStatement("SELECT  Id, FirstName, LastName, Email,PostalCode, Phone, Country FROM Customer");
+
+            //Excute Query
+            ResultSet resultSet = ps.executeQuery();
+            while (resultSet.next()) {
+                customers.add(
+                        new Customers(
+                                resultSet.getString("Id"),
+                                resultSet.getString("FirstName"),
+                                resultSet.getString("LastName"),
+                                resultSet.getString("Country"),
+                                resultSet.getString("PostalCode"),
+                                resultSet.getString("Phone"),
+                                resultSet.getString("Email")
+                        )
+                );
+            }
+            log("Selected all customers successfully");
+
+        } catch (Exception e) {
+            log(e.toString());
+        } finally {
+            try {
+                conn.close();
+            } catch (Exception e) {
+                log(e.toString());
+            }
+        }
+        return customers;
+    }
+
+    private static void log(String message) {
+        DateFormat dateFormat = new SimpleDateFormat("\"yyyy/MM/dd HH:mm:ss\"");
+        Date date = new Date();
+        System.out.println(dateFormat.format(date) + " : " + message);
+    }
+
+}
---
+++ src/main/java/com/example/demo/models/Customers.java
@@ -0,0 +1,77 @@
+package com.example.demo.models;
+
+public class Customers {
+    private String CustomerId;
+    private String FirstName;
+    private String LastName;
+    private String Country;
+    private String PostalCode;
+    private String Phone;
+    private String Email;
+
+    public Customers(String customerId, String firstName, String lastName, String country, String postalCode, String phone, String email) {
+        this.CustomerId = customerId;
+        this.FirstName = firstName;
+        this.LastName = lastName;
+        this.Country = country;
+        this.PostalCode = postalCode;
+        this.Phone = phone;
+        this.Email = email;
+    }
+
+    public String getCustomerId() {
+        return CustomerId;
+    }
+
+    public void setCustomerId(String customerId) {
+        this.CustomerId = customerId;
+    }
+
+    public String getFirstName() {
+        return FirstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.FirstName = firstName;
+    }
+
+    public String getLastName() {
+        return LastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.LastName = lastName;
+    }
+
+    public String getCountry() {
+        return Country;
+    }
+
+    public void setCountry(String country) {
+        this.Country = country;
+    }
+
+    public String getPostalCode() {
+        return PostalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.PostalCode = postalCode;
+    }
+
+    public String getPhone() {
+        return Phone;
+    }
+
+    public void setPhone(String phone) {
+        this.Phone = phone;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String email) {
+        this.Email = email;
+    }
+}
